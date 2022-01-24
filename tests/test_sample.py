ace430ec969692cceb904f8d14f47483458fee31
Mike Prior <priormike@gmail.com>
2022-01-24 13:39:31 -0600

    Delete tests directory

--- tests/test_sample.py
+++
@@ -1,8 +0,0 @@
-from school import *
-from helpers import *
-
-def test_school_name():
-    s = School
-    current_school = s.get_school_list()[0]
-    assert(current_school.SchoolName == "ANKENY HIGH SCHOOL")
-
